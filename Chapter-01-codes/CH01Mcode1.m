7d3631486fb407091486d2f32ffc6574924467b2
prog871 <165429386+prog871@users.noreply.github.com>
2025-09-20 23:56:59 +0100

    Add files via upload

---
+++ Chapter-01-codes/CH01Mcode1.m
@@ -0,0 +1,14 @@
+% start of the M - file
+%this M-file, named CH01Mcode1.m,
+%takes three values and returns
+% sum of their squares
+clc ; % clear screen
+clear ;%clear all variables from memory
+a = -4;
+b = 1.5;
+c = 0.25;
+% calculations
+s = a ^2 + b ^2+ c ^2;
+% returning the results in the screen
+display ( s )
+% end of M - file
